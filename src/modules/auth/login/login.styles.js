e6968aab222ba0d21eea74c001de50a99fc9e2b5
Vinay9720 <131252201+Vinay9720@users.noreply.github.com>
2023-09-27 03:57:12 +0530

    SWX-225--Login page UI Updated

--- src/modules/auth/login/login.styles.js
+++ src/modules/auth/login/login.styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
     display: flex;
     justify-content: center;
     min-height: 100vh;
-    widht: 100%;
+    width: 100%;
     background-position: center;
     background-size: cover;
     background-image: url('/images/login_background.jpg');
@@ -15,27 +15,29 @@ export const StyledLoginContainer = styled.div`
     flex-direction: column;
     width: 30rem;
     padding: 2rem;
-    gap: 2rem;
     align-self: center;
 `;
 
 export const HeadingContainer = styled.div`
     display: flex;
-    gap: 1rem;
+    gap: 10px;
     align-items: center;
     justify-content: center;
     flex-direction: column;
+    margin-bottom: 32px;
 `;
 
 export const FooterContainer = styled.div`
     display: flex;
-    gap: 8px;
+    flex-direction: column;
+    gap: 40px;
     align-items: center;
     justify-content: center;
     word-wrap: break-word;
     font-size: 16px;
     color: #ffffff;
     cursor: pointer;
+    margin-top: 24px;
 `;
 
 export const IconContainer = styled.div`
@@ -44,6 +46,7 @@ export const IconContainer = styled.div`
     justify-content: center;
     align-items: center;
     cursor: pointer;
+    margin-top: 40px;
 `;
 
 export const CopyrightContainer = styled.div`
@@ -52,4 +55,5 @@ export const CopyrightContainer = styled.div`
     color: #ffffff;
     cursor: pointer;
     text-align: center;
+    margin-top: 52px;
 `;
