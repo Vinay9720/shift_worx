f145d68bc110b0ff15e380c35a0ffb8c0498fc79
Navin Malviya <navin@nurseio.com>
2024-02-06 18:00:55 +0530

    Monthly delete fix

--- src/hooks/admin-schedule-templates/useDeleteTemplateShift.js
+++ src/hooks/admin-schedule-templates/useDeleteTemplateShift.js
@@ -5,7 +5,7 @@ import AdminScheduleTemplatesService from '@/services/admin-schedule-templates';
 import { closeModal } from '@/lib/store/slices/modal-slice';
 
 import { useToast } from '../common';
-import { clearState } from '@/lib/store/slices/admin-schedule-templates-module';
+import { clearState, setTemplateShiftTobeDeleted } from '@/lib/store/slices/admin-schedule-templates-module';
 
 export const useDeleteTemplateShift = () => {
     const queryClient = useQueryClient();
@@ -20,6 +20,7 @@ export const useDeleteTemplateShift = () => {
     return useMutation(deleteTemplateShift, {
         onSuccess: async () => {
             queryClient.invalidateQueries('admin-schedule-template');
+            dispatch(setTemplateShiftTobeDeleted({}));
             dispatch(closeModal({ modalName: 'deleteTemplateShiftModal' }));
             dispatch(clearState());
             showToast('Shift Successfully Deleted!', 'success');
